694c7e427de668ac9d203ac5f3e6458a5d14526c
Alex <alvastarua@gmail.com>
2020-01-17 20:55:05 +0200

    Lesson 11

---
+++ lesson-11/validation/controller.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+header('Content-Type: text/plain');
+
+if (isset($_POST['year'], $_POST['month'], $_POST['day'])) {
+
+    $year = (int)$_POST['year'];
+    $month = (int)$_POST['month'];
+    $day = (int)$_POST['day'];
+
+    $requiredTime = strtotime('-18 years');
+    $userTime = strtotime("{$year}.{$month}.{$day}");
+
+    if ($userTime < $requiredTime) {
+        // Проходи
+    } else {
+        $_SESSION['errors'] = [
+            'age' => [
+                'value' => $year,
+                'message' => 'Доступ запрещен!'
+            ]
+        ];
+
+        header('Location: /lesson-11/validation/form.phtml');
+    }
+}
