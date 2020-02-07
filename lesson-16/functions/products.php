7b3db2fe3a94c26d341370b2ee7740c5b2aaafe5
Alex <alvastarua@gmail.com>
2020-02-07 21:06:36 +0200

    Get products data

--- lesson-16/functions/products.php
+++ lesson-16/functions/products.php
@@ -1,30 +1,76 @@
 <?php
 
+require_once __DIR__ . '/database.php';
+
 function get_product(int $id): array
 {
-    $query = 'SELECT * FROM products
-                INNER JOIN category_attributes
-                    ON category_attributes.category_id = products.category_id
-                    
-                LEFT JOIN  product_attributes_int
-                    ON product_attributes_int.product_id = products.id
-                   AND product_attributes_int.attribute_id = category_attributes.attribute_id
-                   
-                LEFT JOIN  product_attributes_decimal
-                    ON product_attributes_decimal.product_id = products.id
-                   AND product_attributes_decimal.attribute_id = category_attributes.attribute_id
-                   
-                LEFT JOIN  product_attributes_varchar
-                    ON product_attributes_varchar.product_id = products.id
-                   AND product_attributes_varchar.attribute_id = category_attributes.attribute_id
-                   
-                LEFT JOIN  product_attributes_text
-                    ON product_attributes_text.product_id = products.id
-                   AND product_attributes_text.attribute_id = category_attributes.attribute_id
-                   
-                LEFT JOIN  product_attributes_bool
-                    ON product_attributes_bool.product_id = products.id
-                   AND product_attributes_bool.attribute_id = category_attributes.attribute_id
-                   
-                WHERE products.id = 1';
+    $query = '
+        SELECT 
+            products.id AS product_id,
+            products.sku AS sku,
+            products.category_id AS category_id,
+            attributes.code AS attribute_code,
+            attributes.type as attribute_type,
+            product_attributes_int.value as int_value,
+            product_attributes_decimal.value as decimal_value,
+            product_attributes_varchar.value as varchar_value,
+            product_attributes_text.value as text_value,
+            product_attributes_bool.value as bool_value
+            
+        FROM products
+        
+        INNER JOIN category_attributes
+            ON category_attributes.category_id = products.category_id
+            
+        INNER JOIN attributes
+            ON attributes.id = category_attributes.attribute_id
+            
+        LEFT JOIN  product_attributes_int
+            ON product_attributes_int.product_id = products.id
+           AND product_attributes_int.attribute_id = category_attributes.attribute_id
+           
+        LEFT JOIN  product_attributes_decimal
+            ON product_attributes_decimal.product_id = products.id
+           AND product_attributes_decimal.attribute_id = category_attributes.attribute_id
+           
+        LEFT JOIN  product_attributes_varchar
+            ON product_attributes_varchar.product_id = products.id
+           AND product_attributes_varchar.attribute_id = category_attributes.attribute_id
+           
+        LEFT JOIN  product_attributes_text
+            ON product_attributes_text.product_id = products.id
+           AND product_attributes_text.attribute_id = category_attributes.attribute_id
+           
+        LEFT JOIN  product_attributes_bool
+            ON product_attributes_bool.product_id = products.id
+           AND product_attributes_bool.attribute_id = category_attributes.attribute_id
+           
+        WHERE products.id = ?';
+
+    $statement = get_database()->prepare($query);
+
+    $isSuccess = $statement->execute([$id]);
+    if ($isSuccess === false) {
+        return [];
+    }
+
+    $productData = $statement->fetchAll();
+    if ($productData === false) {
+        return [];
+    }
+
+    $product = [];
+    foreach ($productData as $data) {
+        $key = $data['attribute_code'];
+        $type = $data['attribute_type'];
+        $value = $data[$type . '_value'] ?? null;
+
+        $product[$key] = $value;
+    }
+
+
+    $product['sku'] = $productData[0]['sku'];
+    $product['id'] = $id;
+
+    return $product;
 }
--- lesson-16/index.php
+++ lesson-16/index.php
@@ -1,8 +1,10 @@
 <?php
+header('Content-Type: text/plain');
 
 require_once __DIR__ . '/functions/template.php';
+require_once __DIR__ . '/functions/database.php';
+require_once __DIR__ . '/functions/users.php';
+require_once __DIR__ . '/functions/products.php';
+require_once __DIR__ . '/functions/auth.php';
 
-echo get_template('page/header', [
-    'title' => 'Title'
-]);
-
+var_export(get_product(2));
